d5c4265be686447aba7dfc08933b43a2de4353fa
Isaac7app <ik.appiagyei@gmail.com>
2022-03-16 11:24:47 +0100

    trying to fix so the api request do not update unnecsseraily

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -11,11 +11,14 @@ function Popular () {
     }, []);
 
     const getPopular = async () => {
+        
     const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
     const data = await api.json();
-    setPopular(data.recipes)
-}
 
+    setPopular(data.recipes)
+    console.log(data.recipes)
+ 
+};
   return (
     <div>
         <Wrapper >
