4802b71104d6b525a02a82efaed4d0f0938b559a
Isaac7app <ik.appiagyei@gmail.com>
2022-03-18 10:32:44 +0100

    making a cuisine component functions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import Pages from "./pages/Pages";
 import Category from "./components/Category";
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-      <Category />
-      <Pages />
+        <Category />
+        <Pages />
       </BrowserRouter>
     </div>
   );
