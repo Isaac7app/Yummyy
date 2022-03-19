faeb5940659c1e6ba25980aaadbb44109d1a2cb9
Isaac7app <ik.appiagyei@gmail.com>
2022-03-19 20:43:05 +0100

    adding some stying to the search component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -10,6 +10,16 @@ function Search() {
   )
 }
 
-
+const FormStyle = styled.form`
+   margin :0rem 20rem;
+   position: relative;
+   width: 100%;
+   input {
+       border: none;
+       background: linear-gradient(35deg, #494949, #313131);
+       font-size: 1.5rem;
+       color: white;
+   }
+`;
 
 export default Search
