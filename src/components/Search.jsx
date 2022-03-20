eae16bfa82c6819c0f6ccbc94722d695fc41f949
Isaac7app <ik.appiagyei@gmail.com>
2022-03-20 20:59:42 +0100

    added more styles to the serach component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -19,6 +19,14 @@ const FormStyle = styled.form`
        background: linear-gradient(35deg, #494949, #313131);
        font-size: 1.5rem;
        color: white;
+       padding: 1rem 3rem;
+       border: none;
+       border-radius: 1rem;
+       outline: none;
+   }
+   svg{
+       position: absolute;
+       top: 50%;
    }
 `;
 
