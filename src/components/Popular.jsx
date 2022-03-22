317db23cd8186f91287702ecc8595ac3b939f6a3
Isaac7app <ik.appiagyei@gmail.com>
2022-03-22 08:42:42 +0100

    adding a link so when itemis clicked recipe will be displayed

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import styled from "styled-components";
 import {Splide, SplideSlide} from '@splidejs/react-splide';
 import '@splidejs/splide/dist/css/splide.min.css'; 
+import { Link } from "react-router-dom";
 
 function Popular () {
     const [popular, setPopular] = useState([]);
@@ -41,11 +42,13 @@ function Popular () {
             {popular.map((recipe) => {
                 return(
                     <SplideSlide key={recipe.id}>
-                    <Crad>
-                    <p>{recipe.title}</p> 
-                    <img src={recipe.image} alt={recipe.title} />
-                    <Gradient />
-                    </Crad>
+                     <Crad>
+                      <Link to={'recipe/' + recipe.id}>
+                        <p>{recipe.title}</p> 
+                        <img src={recipe.image} alt={recipe.title} />
+                        <Gradient />
+                      </Link>
+                     </Crad>
                     </SplideSlide>
                 );
             })}
